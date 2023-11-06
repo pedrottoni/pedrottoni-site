3b10f6c0fc5960aae19cc920554362e4909549e0
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2023-11-06 11:34:23 -0300

    mais um teste vercel

--- .vercel/output/static/_astro/hoisted.6a516877.js
+++
@@ -1,1 +0,0 @@
-const t=document.querySelector(".navbar"),c=t.querySelectorAll(".link"),n=window.location.href;c.forEach(e=>{e.href===n&&e.classList.add("active")});document.querySelector(".menuMobile").addEventListener("click",()=>{document.querySelector(".links").classList.toggle("desktop")});
